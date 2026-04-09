3416305fde83bdc864d93265d76001e714952cd7
Amulya <singhamulya09@gmail.com>
2026-04-09 09:27:16 +0530

    feat: add dotenv, express, and mongoose dependencies; set up database connection
    - Updated package.json to include dotenv, express, and mongoose as dependencies.
    - Created constants.js to define the database name.
    - Initialized index.js with mongoose connection logic (currently commented out).
    - Added a new index.js file in the db directory for database-related operations.

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants';
+import connectDB from "./db"
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "ecpress"
+const app = express()
+
+(async ()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("ERROR: ",error);
+        throw error
+       })
+
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+       })
+
+    } catch (error) {
+        console.log("ERROR: ",error)
+        throw err
+    }
+})()
+    */
