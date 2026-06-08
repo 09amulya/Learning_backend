b18f92cc071d64f577928dd39aa161acf93cb581
Amulya <singhamulya09@gmail.com>
2026-06-08 21:08:44 +0530

    feat: add user registration route and async error handling utility

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
  const asyncHandler = (requestHandler) =>{
-    (req, res, next)=> {
+    return(req, res, next)=> {
         Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err))
     }
  }
@@ -29,3 +29,6 @@ export {asyncHandler}
 //         })
 //     }
 // }
+
+
+
